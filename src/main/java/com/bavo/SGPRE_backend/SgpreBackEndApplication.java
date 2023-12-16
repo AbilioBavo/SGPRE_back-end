710d12d590a38b6c999570e5be89f22f5346f200
Abilio Bavo <abiliobavo@gmail.com>
2023-12-16 18:54:18 +0200

    Project created

---
+++ src/main/java/com/bavo/SGPRE_backend/SgpreBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.bavo.SGPRE_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpreBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bavo/SGPRE_backend/SgpreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bavo.SGPRE_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
